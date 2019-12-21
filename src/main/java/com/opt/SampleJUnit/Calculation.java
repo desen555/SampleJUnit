1b875100b951a9f5ead324e6940809c9d9e9463a
desen555 <raman.senthil@gmail.com>
2019-12-20 18:07:59 -0600

    First commit

---
+++ src/main/java/com/opt/SampleJUnit/Calculation.java
@@ -0,0 +1,13 @@
+package com.opt.SampleJUnit;
+
+public class Calculation {
+	// Method to Add
+	public static int add(int a, int b) {
+		return a + b;
+	}
+
+	// Method to substract
+	public static int sub(int a, int b) {
+		return a - b;
+	}
+}
